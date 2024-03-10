84eab196e6e1fff3206aa9997fdc4e779fc65c1c
TechnoWolfTV <146278404+TechnoWolfTV@users.noreply.github.com>
2024-03-10 09:19:08 -0500

    Updating the Backrooms Test Nodes mod and license

--- mods/backroomtest_nodes/main.lua
+++ mods/backroomtest_nodes/main.lua
@@ -26,3 +26,23 @@ minetest.register_node("backroomtest_nodes:wallpaper", {
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2, wood = 1},
 	sounds = default.node_sound_wood_defaults(),
 })
+
+-- Doors
+
+doors.register("backroomtest_nodes:br_door_grey", {
+	tiles = { { name = "br_door_grey.png", backface_culling = true } },
+	description = "Backrooms Test Grey Door",
+	inventory_image = "br_door_item_grey.png",
+	groups = {choppy=2,cracky=2,door=1},
+	protected = false,
+	sounds = default.node_sound_metal_defaults(),
+	sound_open = "doors_steel_door_open",
+	sound_close = "doors_steel_door_close",
+	gain_open = 0.2,
+	gain_close = 0.2,
+	recipe = {
+		{"default:steel_ingot", "default:steel_ingot", ""},
+		{"default:steel_ingot", "default:steel_ingot", "dye:dark_grey"},
+		{"default:steel_ingot", "default:steel_ingot", ""},
+	}
+})
