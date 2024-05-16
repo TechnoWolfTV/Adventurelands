34b3c2728286c380ad55452e3b4e0e94603bb7f8
TechnoWolfTV <146278404+TechnoWolfTV@users.noreply.github.com>
2024-05-16 07:20:58 -0500

    Replaced Whitelist mod by ShadowNinja with one by Zughy

--- mods/whitelist/README.md
+++ mods/whitelist/README.md
@@ -1,22 +1,21 @@
-## Whitelist Mod for [Minetest][]
+# Whitelist
+Filter who can enter a Minetest server and who can't through a whitelist
+
+<a href="https://liberapay.com/Zughy/"><img src="https://i.imgur.com/4B2PxjP.png" alt="Support my work"/></a>  
+
+### Commands
+`/whitelist on`: enables the whitelist  
+`/whitelist off`: disables the whitelist  
+`/whitelist add <player>`: adds <player> to the whitelist (not case sensitive)  
+`/whitelist remove <player>`: removes <player> from the whitelist (not case sensitive)  
+`/whitelist who`: lists all the whitelisted players
+
+### API
+Have a look at the [DOCS](https://gitlab.com/zughy-friends-minetest/whitelist/-/blob/master/DOCS.md)
 
 
----
-### **Description:**
-
-This is a simple Minetest mod that adds a player whitelist. Any player names not found in the whitelist are denied access to the server.
-
-- Commands:
-  - ***/whitelist add <name>***
-    - Adds a username to whitelist & allows access to server.
-  - ***/whitelist remove <name>***
-    - Removes a username from whitelist & denies access to server.
-
-
----
-### **Licensing:**
-
-WTFPL
-
-
-[Minetest]: http://www.minetest.net/
+### Want to help?
+Feel free to:
+* open an [issue](https://gitlab.com/zughy-friends-minetest/whitelist/-/issues)
+* submit a merge request. In this case, PLEASE, do follow milestones and my [coding guidelines](https://cryptpad.fr/pad/#/2/pad/view/-l75iHl3x54py20u2Y5OSAX4iruQBdeQXcO7PGTtGew/embed/). I won't merge features for milestones that are different from the upcoming one (if it's declared), nor messy code
+* contact me on the [Minetest Forum](https://forum.minetest.net/memberlist.php?mode=viewprofile&u=26472)
--- mods/whitelist/init.lua
+++ mods/whitelist/init.lua
@@ -1,67 +1,8 @@
---[[
--- Whitelist mod by ShadowNinja
--- License: WTFPL
---]]
+local version = "1.2.3"
 
-local world_path = minetest.get_worldpath()
-local admin = minetest.settings:get("name")
-local whitelist = {}
+dofile(minetest.get_modpath("whitelist") .. "/api.lua")
+dofile(minetest.get_modpath("whitelist") .. "/chatcmdbuilder.lua")
+dofile(minetest.get_modpath("whitelist") .. "/commands.lua")
+dofile(minetest.get_modpath("whitelist") .. "/player_manager.lua")
 
-local function load_whitelist()
-	local file, err = io.open(world_path.."/whitelist.txt", "r")
-	if err then
-		return
-	end
-	for line in file:lines() do
-		whitelist[line] = true
-	end
-	file:close()
-end
-
-local function save_whitelist()
-	local file, err = io.open(world_path.."/whitelist.txt", "w")
-	if err then
-		return
-	end
-	for item in pairs(whitelist) do
-		file:write(item.."\n")
-	end
-	file:close()
-end
-
-load_whitelist()
-
-minetest.register_on_prejoinplayer(function(name, ip)
-	if name == "singleplayer" or name == admin or whitelist[name] then
-		return
-	end
-	return "This server is whitelisted and you are not on the whitelist."
-end)
-
-minetest.register_chatcommand("whitelist", {
-	params = "{add|remove} <nick>",
-	help = "Manipulate the whitelist",
-	privs = {ban=true},
-	func = function(name, param)
-		local action, whitename = param:match("^([^ ]+) ([^ ]+)$")
-		if action == "add" then
-			if whitelist[whitename] then
-				return false, whitename..
-					" is already on the whitelist."
-			end
-			whitelist[whitename] = true
-			save_whitelist()
-			return true, "Added "..whitename.." to the whitelist."
-		elseif action == "remove" then
-			if not whitelist[whitename] then
-				return false, whitename.." is not on the whitelist."
-			end
-			whitelist[whitename] = nil
-			save_whitelist()
-			return true, "Removed "..whitename.." from the whitelist."
-		else
-			return false, "Invalid action."
-		end
-	end,
-})
-
+minetest.log("action", "[WHITELIST] Mod initialised, running version " .. version)
