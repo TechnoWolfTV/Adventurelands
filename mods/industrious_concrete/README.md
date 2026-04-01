c77ae1b84d26b737974c51a9986f8c96fb39e894
TechnoWolfTV <146278404+TechnoWolfTV@users.noreply.github.com>
2026-04-01 09:45:28 -0500

    Adding optional moreblocks and stairs mod support

--- mods/industrious_concrete/README.md
+++ mods/industrious_concrete/README.md
@@ -2,6 +2,7 @@
 *Industrious Mod Originally by StarNinjas - Forked and tweaked by TechnoWolfTV*
 
 Adds new industrious-themed concrete nodes.
+Industrious Concrete also supports Stairs+ from the mod, "Moreblocks" for use in the circular saw, and is an optional dependency.
 
 ### Concrete
 - Powder can be crafted from sand and gravel
--- mods/industrious_concrete/concrete.lua
+++ mods/industrious_concrete/concrete.lua
@@ -1,25 +1,20 @@
---
--- Concrete
---
-
---- SOUNDS
-
+-- -- Concrete --
+-- --- SOUNDS
 local concrete_sound = default.node_sound_stone_defaults({
 	footstep = {name = "industrious_concrete_footstep", gain = 0.15, pitch = 0.9},
 	dig = {name = "industrious_concrete_footstep", gain = 0.4, pitch = 1.1},
 	dug = {name = "industrious_concrete_dug", gain = 0.37},
 })
 
---- CONCRETE
+-- --- CONCRETE
 
 -- Concrete
-
 minetest.register_node("industrious_concrete:concrete", {
 	description = "Concrete",
 	tiles = {"industrious_concrete.png"},
 	groups = {cracky = 2},
 	sounds = concrete_sound,
-	drops = "industrious_concrete:concrete_powder"
+	drop = "industrious_concrete:concrete_powder",
 })
 
 minetest.register_craft({
@@ -33,7 +28,6 @@ minetest.register_craft({
 })
 
 -- Concrete Powder
-
 minetest.register_node("industrious_concrete:concrete_powder", {
 	description = "Concrete Powder",
 	tiles = {"industrious_concrete_powder.png"},
@@ -50,17 +44,19 @@ minetest.register_craft({
 })
 
 -- Concrete with Caution Tape
-
 minetest.register_node("industrious_concrete:concrete_caution", {
 	description = "Concrete with Caution Tape",
 	tiles = {
-		"industrious_concrete_painted_top.png", "industrious_concrete_slab.png",
-		"industrious_concrete_painted_side.png", "industrious_concrete_painted_side.png",
+		"industrious_concrete_painted_top.png",
+		"industrious_concrete_slab.png",
 		"industrious_concrete_painted_side.png",
-		},
+		"industrious_concrete_painted_side.png",
+		"industrious_concrete_painted_side.png",
+		"industrious_concrete_painted_side.png",
+	},
 	groups = {cracky = 2},
 	sounds = concrete_sound,
-	drops = "industrious_concrete:concrete_powder"
+	drop = "industrious_concrete:concrete_powder",
 })
 
 minetest.register_craft({
@@ -72,7 +68,6 @@ minetest.register_craft({
 })
 
 -- Concrete Slab
-
 minetest.register_node("industrious_concrete:concrete_slab", {
 	description = "Concrete Slab",
 	drawtype = "nodebox",
@@ -83,7 +78,7 @@ minetest.register_node("industrious_concrete:concrete_slab", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.5000, -0.5000, -0.5000, 0.5000, 0.000, 0.5000},
+			{-0.5000, -0.5000, -0.5000, 0.5000, 0.0000, 0.5000},
 		},
 	},
 })
@@ -96,16 +91,18 @@ minetest.register_craft({
 })
 
 -- Concrete Block
-
 minetest.register_node("industrious_concrete:concrete_block", {
 	description = "Concrete Block",
 	drawtype = "nodebox",
 	inventory_image = "industrious_concrete_block.png",
 	tiles = {
-		"industrious_concrete_block_top.png", "industrious_concrete_block_top.png",
-		"industrious_concrete.png", "industrious_concrete.png",
+		"industrious_concrete_block_top.png",
+		"industrious_concrete_block_top.png",
 		"industrious_concrete.png",
-		},
+		"industrious_concrete.png",
+		"industrious_concrete.png",
+		"industrious_concrete.png",
+	},
 	paramtype = "light",
 	paramtype2 = "facedir",
 	groups = {cracky = 2, falling_node = 1},
@@ -113,7 +110,7 @@ minetest.register_node("industrious_concrete:concrete_block", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.2500, -0.5000, -0.5000, 0.2500, 0.000, 0.5000},
+			{-0.2500, -0.5000, -0.5000, 0.2500, 0.0000, 0.5000},
 		},
 	},
 })
@@ -126,13 +123,17 @@ minetest.register_craft({
 	}
 })
 
+-- Concrete Block Wall
 minetest.register_node("industrious_concrete:concrete_block_wall", {
 	description = "Concrete Block Wall",
 	tiles = {
-		"industrious_concrete_block_wall_top.png", "industrious_concrete_block_wall_top.png",
-		"industrious_concrete_block_wall_side.png", "industrious_concrete_block_wall_side.png",
+		"industrious_concrete_block_wall_top.png",
+		"industrious_concrete_block_wall_top.png",
 		"industrious_concrete_block_wall_side.png",
-		},
+		"industrious_concrete_block_wall_side.png",
+		"industrious_concrete_block_wall_side.png",
+		"industrious_concrete_block_wall_side.png",
+	},
 	groups = {cracky = 2},
 	sounds = concrete_sound,
 })
@@ -146,7 +147,6 @@ minetest.register_craft({
 })
 
 -- Concrete Brick
-
 minetest.register_craftitem("industrious_concrete:concrete_brick", {
 	description = "Concrete Brick",
 	inventory_image = "industrious_concrete_brick.png",
@@ -159,6 +159,7 @@ minetest.register_craft({
 	}
 })
 
+-- Concrete Brick Wall
 minetest.register_node("industrious_concrete:concrete_brick_wall", {
 	description = "Concrete Brick Wall",
 	tiles = {"industrious_concrete_brick_wall.png"},
@@ -173,3 +174,126 @@ minetest.register_craft({
 		{"industrious_concrete:concrete_brick", "industrious_concrete:concrete_brick"},
 	}
 })
+
+-- --- STAIRS+ SUPPORT (More Blocks circular saw)
+-- Register only these four nodes with Stairs+ / circular saw.
+-- stairsplus:register_all() is the More Blocks API call that also registers
+-- the material with the circular saw.
+if minetest.get_modpath("moreblocks") and stairsplus then
+	stairsplus:register_all("industrious_concrete", "concrete",
+		"industrious_concrete:concrete", {
+			description = "Concrete",
+			tiles = {"industrious_concrete.png"},
+			groups = {cracky = 2},
+			sounds = concrete_sound,
+			drop = "industrious_concrete:concrete_powder",
+		}
+	)
+
+	stairsplus:register_all("industrious_concrete", "concrete_block_wall",
+		"industrious_concrete:concrete_block_wall", {
+			description = "Concrete Block Wall",
+			tiles = {
+				"industrious_concrete_block_wall_top.png",
+				"industrious_concrete_block_wall_top.png",
+				"industrious_concrete_block_wall_side.png",
+				"industrious_concrete_block_wall_side.png",
+				"industrious_concrete_block_wall_side.png",
+				"industrious_concrete_block_wall_side.png",
+			},
+			groups = {cracky = 2},
+			sounds = concrete_sound,
+		}
+	)
+
+	stairsplus:register_all("industrious_concrete", "concrete_brick_wall",
+		"industrious_concrete:concrete_brick_wall", {
+			description = "Concrete Brick Wall",
+			tiles = {"industrious_concrete_brick_wall.png"},
+			groups = {cracky = 2},
+			sounds = concrete_sound,
+		}
+	)
+
+	stairsplus:register_all("industrious_concrete", "concrete_caution",
+		"industrious_concrete:concrete_caution", {
+			description = "Concrete with Caution Tape",
+			tiles = {
+				"industrious_concrete_painted_top.png",
+				"industrious_concrete_slab.png",
+				"industrious_concrete_painted_side.png",
+				"industrious_concrete_painted_side.png",
+				"industrious_concrete_painted_side.png",
+				"industrious_concrete_painted_side.png",
+			},
+			groups = {cracky = 2},
+			sounds = concrete_sound,
+			drop = "industrious_concrete:concrete_powder",
+		}
+	)
+end
+
+-- --- NORMAL STAIRS MOD SUPPORT
+-- These create standard stairs:stair_* and stairs:slab_* craftable nodes.
+-- Unique subnames are used to avoid collisions with other mods.
+if minetest.get_modpath("stairs") and stairs then
+	stairs.register_stair_and_slab(
+		"industrious_concrete_concrete",
+		"industrious_concrete:concrete",
+		{cracky = 2},
+		{"industrious_concrete.png"},
+		"Concrete Stair",
+		"Concrete Slab",
+		concrete_sound,
+		false,
+		"industrious_concrete:concrete_powder"
+	)
+
+	stairs.register_stair_and_slab(
+		"industrious_concrete_concrete_block_wall",
+		"industrious_concrete:concrete_block_wall",
+		{cracky = 2},
+		{
+			"industrious_concrete_block_wall_top.png",
+			"industrious_concrete_block_wall_top.png",
+			"industrious_concrete_block_wall_side.png",
+			"industrious_concrete_block_wall_side.png",
+			"industrious_concrete_block_wall_side.png",
+			"industrious_concrete_block_wall_side.png",
+		},
+		"Concrete Block Wall Stair",
+		"Concrete Block Wall Slab",
+		concrete_sound,
+		false
+	)
+
+	stairs.register_stair_and_slab(
+		"industrious_concrete_concrete_brick_wall",
+		"industrious_concrete:concrete_brick_wall",
+		{cracky = 2},
+		{"industrious_concrete_brick_wall.png"},
+		"Concrete Brick Wall Stair",
+		"Concrete Brick Wall Slab",
+		concrete_sound,
+		false
+	)
+
+	stairs.register_stair_and_slab(
+		"industrious_concrete_concrete_caution",
+		"industrious_concrete:concrete_caution",
+		{cracky = 2},
+		{
+			"industrious_concrete_painted_top.png",
+			"industrious_concrete_slab.png",
+			"industrious_concrete_painted_side.png",
+			"industrious_concrete_painted_side.png",
+			"industrious_concrete_painted_side.png",
+			"industrious_concrete_painted_side.png",
+		},
+		"Concrete with Caution Tape Stair",
+		"Concrete with Caution Tape Slab",
+		concrete_sound,
+		false,
+		"industrious_concrete:concrete_powder"
+	)
+end
